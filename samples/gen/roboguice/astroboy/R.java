33e1055dcaef50126ad6fbca07093c79356b1f31
mike <none@none>
2009-10-22 10:35:47 -0700

    inject resources

--- samples/gen/roboguice/astroboy/R.java
+++ samples/gen/roboguice/astroboy/R.java
@@ -13,6 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int widget1=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
--- samples/src/roboguice/astroboy/activity/DoctorTenma.java
+++ samples/src/roboguice/astroboy/activity/DoctorTenma.java
@@ -2,15 +2,21 @@ package roboguice.astroboy.activity;
 
 import roboguice.activity.GuiceActivity;
 import roboguice.astroboy.R;
+import roboguice.inject.InjectResource;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DoctorTenma extends GuiceActivity {
+    @InjectResource(R.id.widget1) protected TextView helloView;
+    @InjectResource(R.string.hello) protected String hello;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main); // Injection doesn't happen until you call setContentView()
+
+        helloView.setText( hello + ", " + this.getClass().getSimpleName() );
     }
 
 }

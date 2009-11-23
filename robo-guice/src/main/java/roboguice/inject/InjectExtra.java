ada3998e56584720ba0e4f6e81f222f29107caa6
py.ricau@gmail.com <py.ricau@gmail.com>
2009-11-23 12:09:47 +0100

    Javadoc on InjectExtra and InjectResource

--- robo-guice/src/main/java/roboguice/inject/InjectExtra.java
+++ robo-guice/src/main/java/roboguice/inject/InjectExtra.java
@@ -8,8 +8,18 @@ import java.lang.annotation.Target;
 
 import com.google.inject.BindingAnnotation;
 
+/**
+ * Indicates that a variable member of a class (whether static or not) should be injected with an Android extra. The
+ * value is compulsory, and correspond to the key of the extra.<br />
+ * <br />
+ * You can use this annotation combined with @Nullable, or @DefaultXXX to set the default value when trying to get the
+ * extra (XXX being a wrapper type, @DefaultBoolean for isntance)<br />
+ * <br />
+ * Usage example:<br /> {@code @InjectExtra("someValue") protected Integer someValue;}<br />
+ * <br />{@code @InjectExtra("someValue") @DefaultInteger(2) protected Integer someValue;}
+ */
 @Retention(RUNTIME)
-@Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
+@Target( { ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
 @BindingAnnotation
 public @interface InjectExtra {
     String value();
--- robo-guice/src/main/java/roboguice/inject/InjectResource.java
+++ robo-guice/src/main/java/roboguice/inject/InjectResource.java
@@ -8,8 +8,13 @@ import java.lang.annotation.Target;
 
 import com.google.inject.BindingAnnotation;
 
+/**
+ * Indicates that a variable member of a class (whether static or not) should be injected with an Android resource. The
+ * value is compulsory, and correspond to the id of the resource.<br />
+ * Usage example:<br /> {@code @InjectResource(R.string.hello) protected String hello;}
+ */
 @Retention(RUNTIME)
-@Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
+@Target( { ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
 @BindingAnnotation
 public @interface InjectResource {
     int value();

fc19cb2b4201c1bf89f9206ccc2e980a9d69c31e
mike <none@none>
2010-03-30 17:38:18 -0400

    test case that subclasses roboasynctask to do something

--- tests/src/roboguice/test/SafeAsyncTest.java
+++ tests/src/roboguice/test/SafeAsyncTest.java
@@ -1,12 +1,22 @@
 package roboguice.test;
 
+import org.apache.http.client.HttpClient;
 import roboguice.test.config.RoboGuiceTestApplication;
 import roboguice.util.RoboAsyncTask;
 import roboguice.util.RoboLooperThread;
 
+import android.content.Context;
+import android.os.Handler;
 import android.test.suitebuilder.annotation.MediumTest;
+import android.view.View;
 
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.net.URI;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadFactory;
+
 
 public class SafeAsyncTest extends RoboUnitTestCase<RoboGuiceTestApplication> {
     public enum State {
@@ -233,4 +243,97 @@ public class SafeAsyncTest extends RoboUnitTestCase<RoboGuiceTestApplication> {
         assertNull(state[0], state[0]);
     }
 
+
+
+    @MediumTest
+    public void testFakeHttp() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final State state[] = new State[]{State.UNKNOWN};
+
+        new RoboLooperThread() {
+            public void run() {
+
+                new FakeHttpGet(new View(getInjector().getInstance(Context.class)), URI.create("http://google.com")){
+                    @Override
+                    protected void onSuccess(String s) throws Exception {
+                        state[0] = s.contains("booga") ? SafeAsyncTest.State.TEST_SUCCESS : SafeAsyncTest.State.TEST_FAIL;
+                    }
+
+                    @Override
+                    protected void onFinally() {
+                        latch.countDown();
+                    }
+
+                }.execute();
+
+            }
+        }.start();
+
+
+        latch.await();
+        assertEquals(State.TEST_SUCCESS,state[0]);
+    }
+
+
+
 }
+
+
+
+
+
+
+
+/**
+ * Copied from another project, using here for test purposes
+ */
+class FakeHttpGet extends RoboAsyncTask<String> {
+    // We use static injection here so that it's easy for users of HttpGet
+    // to instantiate instances without having to do injection themselves.
+    @Inject static protected Provider<HttpClient> client;
+
+    protected View spinner;
+    protected URI uri;
+
+    public FakeHttpGet( URI uri ) {
+        this.uri = uri;
+    }
+
+    public FakeHttpGet( View spinner, URI uri ) {
+        this.spinner = spinner;
+        this.uri = uri;
+    }
+
+    public FakeHttpGet(Handler handler, View spinner, URI uri) {
+        super(handler);
+        this.spinner = spinner;
+        this.uri = uri;
+    }
+
+    public FakeHttpGet(ThreadFactory threadFactory, Handler handler, View spinner, URI uri) {
+        super(handler, threadFactory);
+        this.spinner = spinner;
+        this.uri = uri;
+    }
+
+
+    public String call() throws Exception {
+        return "<html>booga</html>";
+    }
+
+
+    @Override
+    protected void onPreExecute() throws Exception {
+        if( spinner != null )
+            spinner.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onFinally() {
+        if( spinner != null )
+            spinner.setVisibility(View.GONE);
+    }
+
+}
+
+

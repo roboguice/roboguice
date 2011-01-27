666baa4516d145ea1326f0c2c6bd13fd18f3ba7f
mike <none@none>
2011-01-26 20:56:08 -0500

    Check in missing file

---
+++ roboguice/src/main/java/roboguice/RoboGuice.java
@@ -0,0 +1,55 @@
+package roboguice;
+
+import roboguice.application.RoboApplication;
+import roboguice.config.RoboModule;
+
+import android.app.Application;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+
+import java.util.ArrayList;
+import java.util.WeakHashMap;
+
+public class RoboGuice {
+    protected static WeakHashMap<Application,Injector> injectors = new WeakHashMap<Application,Injector>();
+
+    private RoboGuice() {
+    }
+
+    public static Injector createInjector(Application context) {
+
+        Injector rtrn = injectors.get(context);
+        if( rtrn!=null )
+            return rtrn;
+
+        synchronized (RoboGuice.class) {
+            rtrn = injectors.get(context);
+            if( rtrn!=null )
+                return rtrn;
+
+            final int id = context.getResources().getIdentifier("roboguice_modules", "array", context.getPackageName());
+            final String[] moduleNames = context.getResources().getStringArray(id);
+            final ArrayList<Module> modules = new ArrayList<Module>();
+
+            modules.add(new RoboModule((RoboApplication) context));
+
+            if (moduleNames != null) {
+                try {
+                    for (String name : moduleNames)
+                        modules.add((Module) Class.forName(name).newInstance());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            rtrn = Guice.createInjector(Stage.PRODUCTION, modules);
+            injectors.put(context,rtrn);
+
+        }
+
+        return rtrn;
+    }
+}

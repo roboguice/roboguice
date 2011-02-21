4dae3185af6e4f7518ed749b66324c8c2f4d0834
mike <none@none>
2011-02-21 11:20:36 -0800

    formatting
    --HG--
    branch : roboguice-1.1-branch

--- roboguice/src/main/java/roboguice/activity/event/OnConfigurationChangedEvent.java
+++ roboguice/src/main/java/roboguice/activity/event/OnConfigurationChangedEvent.java
@@ -10,19 +10,19 @@ import android.content.res.Configuration;
  */
 public class OnConfigurationChangedEvent {
 
-        protected Configuration oldConfig;
-        protected Configuration newConfig;
+    protected Configuration oldConfig;
+    protected Configuration newConfig;
 
-        public OnConfigurationChangedEvent(Configuration oldConfig, Configuration newConfig) {
-            this.oldConfig = oldConfig;
-            this.newConfig = newConfig;
-        }
-
-        public Configuration getOldConfig() {
-            return oldConfig;
-        }
-
-        public Configuration getNewConfig() {
-            return newConfig;
-        }
+    public OnConfigurationChangedEvent(Configuration oldConfig, Configuration newConfig) {
+        this.oldConfig = oldConfig;
+        this.newConfig = newConfig;
     }
+
+    public Configuration getOldConfig() {
+        return oldConfig;
+    }
+
+    public Configuration getNewConfig() {
+        return newConfig;
+    }
+}

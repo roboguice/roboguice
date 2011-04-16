f2df28c3f8001fd28daba21371bd7829fb9855bc
mike <none@none>
2011-04-16 15:38:47 -0400

    attempt to add non-working inject prefs testcase
    --HG--
    branch : guice30

--- roboguice/src/test/java/roboguice/activity/ActivityInjectionTest.java
+++ roboguice/src/test/java/roboguice/activity/ActivityInjectionTest.java
@@ -3,18 +3,26 @@ package roboguice.activity;
 import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import roboguice.inject.InjectExtra;
+import roboguice.inject.InjectPreference;
 import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 
 import android.R;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
 import android.widget.TextView;
 
 import com.google.inject.Inject;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Constructor;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -23,12 +31,16 @@ import static org.junit.Assert.assertThat;
 public class ActivityInjectionTest {
 
     protected DummyActivity activity;
+    protected DummyPreferenceActivity prefsActivity;
     
     @Before
     public void setup() {
         activity = new DummyActivity();
         activity.setIntent( new Intent(Robolectric.application,DummyActivity.class).putExtra("foobar","goober") );
         activity.onCreate(null);
+
+        prefsActivity = new DummyPreferenceActivity();
+        prefsActivity.onCreate(null);
     }
 
     @Test
@@ -51,6 +63,11 @@ public class ActivityInjectionTest {
         assertThat(activity.foobar,is("goober"));
     }
 
+    // BUG This doesn't work yet because createNewPreferenceScreen doesn't properly model whatever's goign on
+    @Test @Ignore
+    public void shouldInjectPreference() {
+        assertThat(prefsActivity.pref, is(prefsActivity.findPreference("xxx")));
+    }
 
 
     public static class DummyActivity extends RoboActivity {
@@ -66,6 +83,43 @@ public class ActivityInjectionTest {
             final TextView root = new TextView(this);
             root.setId(R.id.text1);                       
             setContentView(root);
+        }
+    }
+
+    public static class DummyPreferenceActivity extends RoboPreferenceActivity {
+        @Nullable @InjectPreference("xxx") protected Preference pref;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            final PreferenceScreen screen = createNewPreferenceScreen();
+
+            final Preference p = new CheckBoxPreference(this);
+            p.setKey("xxx");
+
+            screen.addPreference(p);
+
+            setPreferenceScreen(screen);
+        }
+
+        protected PreferenceScreen createNewPreferenceScreen() {
+
+            try {
+                final Constructor<PreferenceScreen> c = PreferenceScreen.class.getDeclaredConstructor();
+                c.setAccessible(true);
+                final PreferenceScreen screen = c.newInstance();
+
+                /*
+                final Method m = PreferenceScreen.class.getMethod("onAttachedToHierarchy");
+                m.setAccessible(true);
+                m.invoke(this);
+                */
+
+                return screen;
+                
+            } catch( Exception e ) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }

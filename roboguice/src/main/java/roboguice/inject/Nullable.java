7b829997c22b1be346700621a756f290b1835164
mike <none@none>
2011-10-13 22:01:49 +0200

    add private constructor

--- roboguice/src/main/java/roboguice/inject/Nullable.java
+++ roboguice/src/main/java/roboguice/inject/Nullable.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
 public class Nullable {
+    private Nullable() {
+    }
+    
     public static boolean notNullable( Field field ) {
         return !isNullable( field );
     }

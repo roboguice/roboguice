1e4d5471ccbc9d3a1b02a5e6a3924bcd2d4afe33
Mike Burton <mburton@groupon.com>
2013-07-25 15:42:20 -0700

    Test exception handling

--- roboguice/src/test/java/roboguice/util/AndroidCallableTest.java
+++ roboguice/src/test/java/roboguice/util/AndroidCallableTest.java
@@ -20,62 +20,37 @@ public class AndroidCallableTest {
 
     @Test
     public void shouldCallMethodsUsingProperThreads() throws Exception {
-
         final Thread fgThread = Thread.currentThread();
         final Thread[] bgThread = {null};
-
-        final ThreadFactory bgThreadFactory = new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable runnable) {
-                bgThread[0] = new Thread(runnable);
-                bgThread[0].setName("bgThread");
-                return bgThread[0];
-            }
-        };
-
-
-        final Thread[] answers = new Thread[4];
-
-        final AndroidCallable c = new AndroidCallable<String>() {
-            @Override
-            public void onPreCall() {
-                answers[0] = Thread.currentThread();
-            }
-
-            @Override
-            public String doInBackground() throws Exception {
-                answers[1] = Thread.currentThread();
-                return "12345";
-            }
-
-            @Override
-            public void onException(Exception e) {
-                // not called
-            }
-
-            @Override
-            public void onSuccess(String result) {
-                answers[2] = Thread.currentThread();
-            }
-
-            @Override
-            public void onFinally() {
-                answers[3] = Thread.currentThread();
-            }
-
-
-        };
-
-        Executors.newSingleThreadExecutor(bgThreadFactory).submit(c);
-
-        // Run all the pending tasks on the ui thread
+        final Thread[] answers = new Thread[5];
         final ShadowLooper looper = Robolectric.shadowOf(Looper.getMainLooper());
 
-        while(answers[3]==null) {
-            looper.runToEndOfTasks();
-        }
+        Executors.newSingleThreadExecutor(new MyThreadFactory(bgThread)).submit(new StringAndroidCallable(answers,false));
 
-        final Thread[] correctAnswer = new Thread[]{fgThread, bgThread[0], fgThread, fgThread };
+        // Run all the pending tasks on the ui thread
+        while(answers[answers.length-1]==null)
+            looper.runToEndOfTasks();
+
+
+        final Thread[] correctAnswer = new Thread[]{fgThread, bgThread[0], null, fgThread, fgThread };
+        assertThat( answers, equalTo(correctAnswer));
+    }
+
+    @Test
+    public void shouldCallMethodsUsingProperThreadsWithException() throws Exception {
+        final Thread fgThread = Thread.currentThread();
+        final Thread[] bgThread = {null};
+        final Thread[] answers = new Thread[5];
+        final ShadowLooper looper = Robolectric.shadowOf(Looper.getMainLooper());
+
+        Executors.newSingleThreadExecutor(new MyThreadFactory(bgThread)).submit(new StringAndroidCallable(answers,true));
+
+        // Run all the pending tasks on the ui thread
+        while(answers[answers.length-1]==null)
+            looper.runToEndOfTasks();
+
+
+        final Thread[] correctAnswer = new Thread[]{fgThread, bgThread[0], fgThread, null, fgThread };
         assertThat( answers, equalTo(correctAnswer));
     }
 
@@ -91,4 +66,60 @@ public class AndroidCallableTest {
         throw new UnsupportedOperationException();
     }
 
+    private static class StringAndroidCallable extends AndroidCallable<String> {
+        Thread[] answers;
+        boolean shouldThrowException;
+
+        public StringAndroidCallable(Thread[] answers, boolean shouldThrowException ) {
+            this.answers = answers;
+            this.shouldThrowException = shouldThrowException;
+        }
+
+        @Override
+        public void onPreCall() {
+            answers[0] = Thread.currentThread();
+        }
+
+        @Override
+        public String doInBackground() throws Exception {
+            answers[1] = Thread.currentThread();
+
+            if( shouldThrowException )
+                throw new UnsupportedOperationException();
+
+            return "12345";
+        }
+
+        @Override
+        public void onException(Exception e) {
+            answers[2] = Thread.currentThread();
+        }
+
+        @Override
+        public void onSuccess(String result) {
+            answers[3] = Thread.currentThread();
+        }
+
+        @Override
+        public void onFinally() {
+            answers[4] = Thread.currentThread();
+        }
+
+
+    }
+
+    private static class MyThreadFactory implements ThreadFactory {
+        private final Thread[] bgThread;
+
+        public MyThreadFactory(Thread[] bgThread) {
+            this.bgThread = bgThread;
+        }
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+            bgThread[0] = new Thread(runnable);
+            bgThread[0].setName("bgThread");
+            return bgThread[0];
+        }
+    }
 }

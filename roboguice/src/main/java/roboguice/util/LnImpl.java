93035ae5b990d94e5b78fa36578116aff5e5f6dc
Mike Burton <mburton@groupon.com>
2013-07-31 11:37:47 -0700

    Get rid of Ln.Config and Ln.Print https://github.com/roboguice/roboguice/pull/16#issuecomment-21648121

--- roboguice/src/main/java/roboguice/util/LnImpl.java
+++ roboguice/src/main/java/roboguice/util/LnImpl.java
@@ -6,158 +6,169 @@ import android.util.Log;
 import com.google.inject.Inject;
 
 public class LnImpl implements LnInterface {
-    /**
-     * config is initially set to BaseConfig() with sensible defaults, then replaced
-     * by BaseConfig(ContextSingleton) during guice static injection pass.
-     */
-    @Inject(optional = true) protected Ln.Config config = new BaseConfig();
 
-    /**
-     * print is initially set to Print(), then replaced by guice during
-     * static injection pass.  This allows overriding where the log message is delivered to.
-     */
-    @Inject(optional = true) protected Ln.Print print = new BasePrint();
+    protected int minimumLogLevel = Log.VERBOSE;
+    protected String packageName = "";
+    protected String tag = "";
+
+    public LnImpl() {
+        // do nothing, used by Ln before injection is set up
+    }
+
+    @Inject
+    public LnImpl(Application context) {
+        try {
+            packageName = context.getPackageName();
+            final int flags = context.getPackageManager().getApplicationInfo(packageName, 0).flags;
+            minimumLogLevel = (flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0 ? Log.VERBOSE : Log.INFO;
+            tag = packageName.toUpperCase();
+
+            Ln.d("Configuring Logging, minimum log level is %s", Ln.logLevelToString(minimumLogLevel) );
+
+        } catch( Exception e ) {
+            try {
+                Log.e(packageName, "Error configuring logger", e);
+            } catch( RuntimeException f ) {
+                // HACK ignore Stub! errors in mock objects during testing
+            }
+        }
+    }
 
 
 
     @Override
     public int v(Throwable t) {
-        return config.getLoggingLevel() <= Log.VERBOSE ? print.println(Log.VERBOSE, Log.getStackTraceString(t)) : 0;
+        return getLoggingLevel() <= Log.VERBOSE ? println(Log.VERBOSE, Log.getStackTraceString(t)) : 0;
     }
 
     @Override
     public int v(Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.VERBOSE )
+        if( getLoggingLevel()> Log.VERBOSE )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = args.length>0 ? String.format(s,args) : s;
-        return print.println(Log.VERBOSE, message);
+        return println(Log.VERBOSE, message);
     }
 
     @Override
     public int v(Throwable throwable, Object s1, Object[] args) {
-        if( config.getLoggingLevel()> Log.VERBOSE )
+        if( getLoggingLevel()> Log.VERBOSE )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = (args.length>0 ? String.format(s,args) : s) + '\n' + Log.getStackTraceString(throwable);
-        return print.println(Log.VERBOSE, message);
+        return println(Log.VERBOSE, message);
     }
 
     @Override
     public int d(Throwable t) {
-        return config.getLoggingLevel()<= Log.DEBUG ? print.println(Log.DEBUG, Log.getStackTraceString(t)) : 0;
+        return getLoggingLevel()<= Log.DEBUG ? println(Log.DEBUG, Log.getStackTraceString(t)) : 0;
     }
 
     @Override
     public int d(Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.DEBUG )
+        if( getLoggingLevel()> Log.DEBUG )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = args.length>0 ? String.format(s,args) : s;
-        return print.println(Log.DEBUG, message);
+        return println(Log.DEBUG, message);
     }
 
     @Override
     public int d(Throwable throwable, Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.DEBUG )
+        if( getLoggingLevel()> Log.DEBUG )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = (args.length>0 ? String.format(s,args) : s) + '\n' + Log.getStackTraceString(throwable);
-        return print.println(Log.DEBUG, message);
+        return println(Log.DEBUG, message);
     }
 
     @Override
     public int i(Throwable t) {
-        return config.getLoggingLevel()<= Log.INFO ? print.println(Log.INFO, Log.getStackTraceString(t)) : 0;
+        return getLoggingLevel()<= Log.INFO ? println(Log.INFO, Log.getStackTraceString(t)) : 0;
     }
 
     @Override
     public int i(Throwable throwable, Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.INFO )
+        if( getLoggingLevel()> Log.INFO )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
-        return print.println(Log.INFO, message);
+        return println(Log.INFO, message);
     }
 
     @Override
     public int i(Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.INFO )
+        if( getLoggingLevel()> Log.INFO )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = args.length>0 ? String.format(s,args) : s;
-        return print.println(Log.INFO, message);
+        return println(Log.INFO, message);
     }
 
     @Override
     public int w(Throwable t) {
-        return config.getLoggingLevel()<= Log.WARN ? print.println(Log.WARN, Log.getStackTraceString(t)) : 0;
+        return getLoggingLevel()<= Log.WARN ? println(Log.WARN, Log.getStackTraceString(t)) : 0;
     }
 
     @Override
     public int w(Throwable throwable, Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.WARN )
+        if( getLoggingLevel()> Log.WARN )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = (args.length>0 ? String.format(s,args) : s) + '\n' + Log.getStackTraceString(throwable);
-        return print.println(Log.WARN, message);
+        return println(Log.WARN, message);
     }
 
     @Override
     public int w(Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.WARN )
+        if( getLoggingLevel()> Log.WARN )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = args.length>0 ? String.format(s,args) : s;
-        return print.println(Log.WARN, message);
+        return println(Log.WARN, message);
     }
 
     @Override
     public int e(Throwable t) {
-        return config.getLoggingLevel()<= Log.ERROR ? print.println(Log.ERROR, Log.getStackTraceString(t)) : 0;
+        return getLoggingLevel()<= Log.ERROR ? println(Log.ERROR, Log.getStackTraceString(t)) : 0;
     }
 
     @Override
     public int e(Throwable throwable, Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.ERROR )
+        if( getLoggingLevel()> Log.ERROR )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = (args.length>0 ? String.format(s,args) : s) + '\n' + Log.getStackTraceString(throwable);
-        return print.println(Log.ERROR, message);
+        return println(Log.ERROR, message);
     }
 
     @Override
     public int e(Object s1, Object... args) {
-        if( config.getLoggingLevel()> Log.ERROR )
+        if( getLoggingLevel()> Log.ERROR )
             return 0;
 
         final String s = Strings.toString(s1);
         final String message = args.length>0 ? String.format(s,args) : s;
-        return print.println(Log.ERROR, message);
+        return println(Log.ERROR, message);
     }
 
     @Override
     public boolean isDebugEnabled() {
-        return config.getLoggingLevel()<= Log.DEBUG;
+        return getLoggingLevel()<= Log.DEBUG;
     }
 
     @Override
     public boolean isVerboseEnabled() {
-        return config.getLoggingLevel()<= Log.VERBOSE;
-    }
-
-    @Override
-    public Ln.Config getConfig() {
-        return config;
+        return getLoggingLevel()<= Log.VERBOSE;
     }
 
     @Override
@@ -181,69 +192,33 @@ public class LnImpl implements LnInterface {
     }
 
 
-    public static class BaseConfig implements Ln.Config {
-        protected int minimumLogLevel = Log.VERBOSE;
-        protected String packageName = "";
-        protected String tag = "";
-
-        protected BaseConfig() {
-        }
-
-        @Inject
-        public BaseConfig(Application context) {
-            try {
-                packageName = context.getPackageName();
-                final int flags = context.getPackageManager().getApplicationInfo(packageName, 0).flags;
-                minimumLogLevel = (flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0 ? Log.VERBOSE : Log.INFO;
-                tag = packageName.toUpperCase();
-
-                Ln.d("Configuring Logging, minimum log level is %s", Ln.logLevelToString(minimumLogLevel) );
-
-            } catch( Exception e ) {
-                try {
-                    Log.e(packageName, "Error configuring logger", e);
-                } catch( RuntimeException f ) {
-                    // HACK ignore Stub! errors in mock objects during testing
-                }
-            }
-        }
-
-        public int getLoggingLevel() {
-            return minimumLogLevel;
-        }
-
-        public void setLoggingLevel(int level) {
-            minimumLogLevel = level;
-        }
-
-        public String getTag() {
-            return tag;
-        }
+    public int getLoggingLevel() {
+        return minimumLogLevel;
     }
 
-    public static class BasePrint implements Ln.Print {
-        public int println(int priority, String msg ) {
-            return Log.println(priority, getTag(), processMessage(msg));
-        }
-
-        protected String processMessage(String msg) {
-            if( Ln.getConfig().getLoggingLevel() <= Log.DEBUG )
-                msg = String.format("%s %s", Thread.currentThread().getName(), msg);
-            return msg;
-        }
-
-        protected String getTag() {
-            final int skipDepth = 6; // skip 6 stackframes to find the location where this was called
-            final Ln.Config config = Ln.getConfig();
-            final String tag = config.getTag();
-            if( config.getLoggingLevel() <= Log.DEBUG ) {
-                final StackTraceElement trace = Thread.currentThread().getStackTrace()[skipDepth];
-                return tag + "/" + trace.getFileName() + ":" + trace.getLineNumber();
-            }
-
-            return tag;
-        }
-
+    public void setLoggingLevel(int level) {
+        minimumLogLevel = level;
     }
+
+    public int println(int priority, String msg ) {
+        return Log.println(priority, getTag(), processMessage(msg));
+    }
+
+    protected String processMessage(String msg) {
+        if( getLoggingLevel() <= Log.DEBUG )
+            msg = String.format("%s %s", Thread.currentThread().getName(), msg);
+        return msg;
+    }
+
+    protected String getTag() {
+        final int skipDepth = 6; // skip 6 stackframes to find the location where this was called
+        if( getLoggingLevel() <= Log.DEBUG ) {
+            final StackTraceElement trace = Thread.currentThread().getStackTrace()[skipDepth];
+            return tag + "/" + trace.getFileName() + ":" + trace.getLineNumber();
+        }
+
+        return tag;
+    }
+
 
 }

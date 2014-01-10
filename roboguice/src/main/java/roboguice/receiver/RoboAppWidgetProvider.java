d6d72f2e035251c99dbcddc8784d6ac24541810f
Mike Burton <mburton@groupon.com>
2014-01-10 13:20:45 -0800

    cleanup

--- roboguice/src/main/java/roboguice/receiver/RoboAppWidgetProvider.java
+++ roboguice/src/main/java/roboguice/receiver/RoboAppWidgetProvider.java
@@ -1,21 +1,24 @@
 package roboguice.receiver;
 
 import android.app.Application;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-
 import com.google.inject.Injector;
+import roboguice.RoboGuice;
 
-public class RoboAppWidgetProvider extends AppWidgetProvider {
+public abstract class RoboAppWidgetProvider extends AppWidgetProvider {
 
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	public final void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		final Injector injector = RoboGuice.getBaseApplicationInjector((Application) context.getApplicationContext());
 
 		injector.injectMembers(this);
 		onHandleUpdate(context, appWidgetManager, appWidgetIds);
 	}
 
-	public void onHandleUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-  }
+	@SuppressWarnings("UnusedParameters")
+    public void onHandleUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // proper template method to handle the receive
+    }
 }

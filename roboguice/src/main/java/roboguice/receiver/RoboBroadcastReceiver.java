f8f8b4850806e8c1215d9cb06e7b65481488154d
Mike Burton <mburton@groupon.com>
2014-01-10 13:20:45 -0800

    cleanup

--- roboguice/src/main/java/roboguice/receiver/RoboBroadcastReceiver.java
+++ roboguice/src/main/java/roboguice/receiver/RoboBroadcastReceiver.java
@@ -1,13 +1,10 @@
 package roboguice.receiver;
 
-import roboguice.RoboGuice;
-
 import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import com.google.inject.Injector;
+import roboguice.RoboGuice;
 
 /**
  * To ensure proper ContextScope usage, override the handleReceive method
@@ -21,9 +18,7 @@ public abstract class RoboBroadcastReceiver extends BroadcastReceiver {
      */
     @Override
     public final void onReceive(Context context, Intent intent) {
-        final Injector injector = RoboGuice.getBaseApplicationInjector((Application) context.getApplicationContext());
-
-        injector.injectMembers(this);
+        RoboGuice.getBaseApplicationInjector((Application) context.getApplicationContext()).injectMembers(this);
         handleReceive(context, intent);
     }
 

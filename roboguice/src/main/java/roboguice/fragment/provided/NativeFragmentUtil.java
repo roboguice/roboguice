458c427e7c0b845a6bc3e5e39061d78312dcf12e
Mike Burton <mburton@groupon.com>
2014-03-02 15:53:27 -0800

    Revert "Silver can't handle annotated inner classes, so move these out to the top level"
    This reverts commit 175803d2e0f003b0651927c626574cb477c85ab5.

--- roboguice/src/main/java/roboguice/fragment/provided/NativeFragmentUtil.java
+++ roboguice/src/main/java/roboguice/fragment/provided/NativeFragmentUtil.java
@@ -4,7 +4,6 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 import roboguice.fragment.FragmentUtil.f;
-import roboguice.fragment.support.FragmentManagerProvider;
 import roboguice.inject.ContextSingleton;
 
 import android.annotation.TargetApi;
@@ -61,6 +60,14 @@ public class NativeFragmentUtil implements f<Fragment,FragmentManager> {
 		return FragmentManagerProvider.class;
 	}
 	
+	@ContextSingleton
+	public static class FragmentManagerProvider implements Provider<FragmentManager> {
+	    @Inject protected Activity activity;
+
+	    @Override
+	    public FragmentManager get() {
+	        return activity.getFragmentManager();
+	    }
+	}
 
 }
-

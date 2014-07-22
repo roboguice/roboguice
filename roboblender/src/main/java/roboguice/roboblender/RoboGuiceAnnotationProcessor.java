9cfc55b719c8cfb947aec6251b94dfa1e2c050c1
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-07-22 13:18:39 -0400

    Issue #215. Support latest java version in annotation processor

--- roboblender/src/main/java/roboguice/roboblender/RoboGuiceAnnotationProcessor.java
+++ roboblender/src/main/java/roboguice/roboblender/RoboGuiceAnnotationProcessor.java
@@ -3,7 +3,6 @@ package roboguice.roboblender;
 
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedOptions;
-import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 
 import com.google.inject.blender.GuiceAnnotationProcessor;
@@ -16,7 +15,12 @@ import com.google.inject.blender.GuiceAnnotationProcessor;
  * @author SNI
  */
 @SupportedAnnotationTypes({"com.google.inject.Inject", "com.google.inject.Provides", "javax.inject.Inject", "roboguice.inject.InjectView", "roboguice.inject.InjectResource", "roboguice.inject.InjectPreference", "roboguice.inject.InjectExtra", "roboguice.inject.InjectFragment", "roboguice.event.Observes", "roboguice.inject.ContentView"})
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
 @SupportedOptions({"guiceAnnotationDatabasePackageName"})
 public class RoboGuiceAnnotationProcessor extends GuiceAnnotationProcessor {
+	
+	@Override
+	public SourceVersion getSupportedSourceVersion() {
+		//http://stackoverflow.com/a/8188860/693752
+		return SourceVersion.latest();
+	}
 }

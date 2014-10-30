4d161d40e07b5b42a1bd612910f151be4d0921dd
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-10-30 11:13:25 -0400

    issue-174: Add support for InjectView in custom views

--- additional-tests/src/test/java/roboguice/additionaltests/ViewInjectionTest.java
+++ additional-tests/src/test/java/roboguice/additionaltests/ViewInjectionTest.java
@@ -2,16 +2,16 @@ package roboguice.additionaltests;
 
 import android.content.Context;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import roboguice.activity.RoboActivity;
-import roboguice.additionaltests.view.ShouldInjectCustomViewsView;
+import roboguice.additionaltests.view.CustomViewUnderTest;
 import roboguice.inject.ContentView;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -23,12 +23,23 @@ import static org.junit.Assert.assertThat;
 public class ViewInjectionTest {
 
     @Test
-    @Ignore("Unplugged till Mike reviews it. See RoboActivity.shouldInject...")
-    public void shouldInjectCustomViews() {
+    public void shouldInjectCustomViewsAutomaticallyFromXml() {
         final A a = Robolectric.buildActivity(A.class).create().get();
-        final ShouldInjectCustomViewsView customView = (ShouldInjectCustomViewsView) a.findViewById(R.id.shouldInjectCustomView);
-        assertThat(customView.context, equalTo((Context)a));
+        final CustomViewUnderTest customView = (CustomViewUnderTest) a.findViewById(R.id.shouldInjectCustomView);
+        assertThat(customView.getContext(), equalTo((Context)a));
         assertThat(customView.textView.getId(), equalTo(100));
+        assertThat(customView.textView.getText(), notNullValue());
+        assertThat(customView.vibrator, notNullValue());
+    }
+
+    @Test
+    public void shouldInjectCustomViewsAutomaticallyFromCode() {
+        final A a = Robolectric.buildActivity(A.class).create().get();
+        final CustomViewUnderTest customView = new CustomViewUnderTest(a);
+        assertThat(customView.getContext(), equalTo((Context)a));
+        assertThat(customView.textView.getId(), equalTo(100));
+        assertThat(customView.textView.getText(), notNullValue());
+        assertThat(customView.vibrator, notNullValue());
     }
 
     @ContentView(R.layout.should_inject_custom_views)
---
+++ astroboy/src/main/java/org/roboguice/astroboy/view/TVEvent.java
@@ -0,0 +1,17 @@
+package org.roboguice.astroboy.view;
+
+/**
+ * @author SNI.
+ */
+public class TVEvent {
+
+    private String status;
+
+    public TVEvent(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}

595596d42e2984408ec8fff110d4d088d919ad8a
Stéphane Nicolas <steff.nicolas@gmail.com>
2015-06-26 09:25:08 -0700

    Remove events. RC1 ready

--- roboguice/src/main/java/roboguice/inject/fragment/FragmentManagerProvider.java
+++ roboguice/src/main/java/roboguice/inject/fragment/FragmentManagerProvider.java
@@ -1,19 +1,19 @@
-package roboguice.fragment;
+package roboguice.inject.fragment;
 
-import android.app.Activity;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import android.app.FragmentManager;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+
+import android.app.Activity;
 import roboguice.inject.ContextSingleton;
 
 @ContextSingleton
-public class SupportFragmentManagerProvider implements Provider<FragmentManager> {
+public class FragmentManagerProvider implements Provider<FragmentManager> {
     @Inject protected Activity activity;
 
     @Override
     public FragmentManager get() {
         // BUG only supports compat library at the moment.  Does not support honeycomb directly yet
-        return ((FragmentActivity)activity).getSupportFragmentManager();
+        return activity.getFragmentManager();
     }
 }
--- roboguice/src/main/java/roboguice/inject/fragment/SupportFragmentManagerProvider.java
+++ roboguice/src/main/java/roboguice/inject/fragment/SupportFragmentManagerProvider.java
@@ -1,19 +1,19 @@
-package roboguice.fragment;
-
-import android.app.FragmentManager;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
+package roboguice.inject.fragment;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 import roboguice.inject.ContextSingleton;
 
 @ContextSingleton
-public class FragmentManagerProvider implements Provider<FragmentManager> {
+public class SupportFragmentManagerProvider implements Provider<FragmentManager> {
     @Inject protected Activity activity;
 
     @Override
     public FragmentManager get() {
         // BUG only supports compat library at the moment.  Does not support honeycomb directly yet
-        return activity.getFragmentManager();
+        return ((FragmentActivity)activity).getSupportFragmentManager();
     }
 }
